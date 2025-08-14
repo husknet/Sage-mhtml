f869a9c74c4142e7086dc09c2918067fae742847
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-08-14 11:49:59 -0400

    Delete netlify/edge-functions/go/go.js

--- netlify/edge-functions/go/go.js
+++
@@ -1,9 +0,0 @@
-export default async (request) => {
-  const slug = new URL(request.url).pathname.replace(/^\/go\//, "");
-  const DESTS = {
-    "open-file": "https://www.facebook.com/your-page-or-user/posts/1234567890",
-  };
-  const dest = DESTS[slug];
-  if (!dest) return new Response("Not found", { status: 404 });
-  return Response.redirect(dest, 302);
-};
